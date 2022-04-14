2fd589a3726095b9bc3ef0b97afa0618763c8563
Nicolas Henry <nicolas.henry@gmail.com>
2022-04-14 18:51:35 +0200

    new script to extract tara data

---
+++ ANALYSES/03_tara_data_extract.R
@@ -0,0 +1,50 @@
+library(data.table)
+
+# load eukbank cryptophyceae data
+
+data <- fread(here::here("data","eukbank_18SV4_asv.table_rarefied_10000.gz"))
+
+# load contextual data
+
+context <- fread(here::here("data","eukbank_18SV4_asv.metadata20211123.gz"))
+
+# select Tara samples
+
+context_tara <- context[project=="Tara_Oceans" &
+                          depth_categ == "[SRF] surface water layer (ENVO_00010504)" &
+                          size_fraction_lower_threshold == "0.8" &
+                          size_fraction_upper_threshold == ">0.8" &
+                          !is.na(temperature),
+                        list(sample,latitude,longitude,temperature)]
+
+# load taxo
+
+taxo <- fread(here::here("data","eukbank_18SV4_asv.taxo.gz"))
+
+# add taxo to the asv table
+
+data <- merge(data,
+              taxo[,.(amplicon,similarity,taxonomy,references)],
+              by="amplicon")
+
+# subset the asv table
+data_tara <- data[,.SD,.SDcols=c(context_tara[,sample])]
+x <- apply(data_tara,1,sum)
+y <- apply(data_tara,1,function(X) sum(X>0))
+
+data_tara <- data.table(data[,.(amplicon,similarity,taxonomy,references)],
+                        total=x,
+                        spread=y,
+                        data_tara)
+
+data_tara <- data_tara[total >= 10 & spread >= 2]
+
+fwrite(data_tara,
+       file = here::here("outputs","tara_data_asv_table.tsv.gz"),
+       sep="\t",
+       quote=FALSE)
+
+fwrite(context_tara,
+       file = here::here("outputs","tara_data_context.tsv.gz"),
+       sep="\t",
+       quote=FALSE)
